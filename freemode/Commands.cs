d4dfe3303b397187b3a172f1ece15d288f912d4f
manol <manol@DESKTOP-PT0GKA2>
2022-07-28 12:40:38 +0200

    Add project files.

---
+++ freemode/Commands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace freemode
+{
+    class Commands : Script
+    {
+        [Command("veh", "/veh спавнит авто в координатах игрока", Alias = "vehicle")]
+        private void cmd_veh(Player player, string vehname, int color1, int color2)
+        { 
+            uint vhash = NAPI.Util.GetHashKey(vehname);
+            if(vhash <= 0)
+            {
+                player.SendChatMessage("∼r∼Неверная модель т/с");
+            }
+            Vehicle veh = NAPI.Vehicle.CreateVehicle(vhash, player.Position, player.Heading, color1, color2);
+            veh.NumberPlate = "PIDORAS";
+            veh.Locked = false;
+            veh.EngineStatus = true;
+            player.SetIntoVehicle(veh, (int)VehicleSeat.Driver);
+        }
+
+    }
+}
---
+++ freemode/Events.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace freemode
+{
+    class Events : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        private void OnPlayerConnected(Player player)
+        {
+            player.SendChatMessage("Добро пожаловать на ∼c∼PidorasRP!");
+        }
+
+        [ServerEvent(Event.PlayerSpawn)]
+        private void OnPlayerSpawn(Player player)
+        {
+            player.Health = 100;
+            player.Armor = 0;
+        }   
+    }
+}
